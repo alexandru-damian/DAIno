a7b8d7bd6d8b15f0f5b156d3c24c6665548961c0
Alex Damian <alexdamian121@yahoo.com>
2019-06-01 02:15:25 +0300

    Rendered default dummy rectangle game character

--- game/game.js
+++ game/game.js
@@ -1,12 +1,19 @@
 var lastRender = 0;
 var canv,ctx;
 
+var player_x = 0;
+var player_y = 0;
+
+function load_default_assests()
+{
+	player_x = 30;
+	player_y = 200;
+}
+
 function build_config()
 {
 	canv = document.getElementById("game");
 	ctx = canv.getContext("2d");
-
-
 }
 
 function update(progress)
@@ -16,7 +23,7 @@ function update(progress)
 
 function render()
 {
-
+	ctx.fillRect(player_x, player_y, 30, 50);
 }
 
 function gameloop(timestamp)
@@ -34,6 +41,7 @@ function gameloop(timestamp)
 function generate_scene()
 {
 	build_config();
+	load_default_assests();
 	window.requestAnimationFrame(gameloop)
 }
 
