9ce45919e45501fd7930aad9b6fd98cd60f328c0
Alex Damian <alexdamian121@yahoo.com>
2019-06-01 01:52:21 +0300

    Created the gameloop basic logic

--- game/game.js
+++ game/game.js
@@ -1,15 +1,40 @@
+var lastRender = 0;
+var canv,ctx;
 
 function build_config()
 {
-	var canv = document.getElementById("game");
-	var ctx = canv.getContext("2d");
+	canv = document.getElementById("game");
+	ctx = canv.getContext("2d");
+
+
 }
 
+function update(progress)
+{
+	console.log(progress);
+}
 
+function render()
+{
+
+}
+
+function gameloop(timestamp)
+{
+	var progress = timestamp - lastRender;
+
+	update(progress);
+	render();
+
+	lastRender = timestamp;
+
+	window.requestAnimationFrame(gameloop)
+}
 
 function generate_scene()
 {
 	build_config();
+	window.requestAnimationFrame(gameloop)
 }
 
 
