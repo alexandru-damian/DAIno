656809cfe410b6a8aadab83f7d591f244e268fd6
Alex Damian <alexdamian121@yahoo.com>
2019-06-01 11:59:07 +0300

    Added basic parameters for game character

--- game/game.js
+++ game/game.js
@@ -4,45 +4,61 @@ var canv,ctx;
 var player_x = 0;
 var player_y = 0;
 
-function load_default_assests()
+var player_height = 0;
+var player_width = 0;
+
+var jump_force = 0;
+var JUMP_THRESHOLD = 40;
+
+function load_player()
 {
-	player_x = 30;
-	player_y = 200;
+    player_width = 30;
+    player_height = 50;
+
+    player_x = 30;
+    player_y = 200;
+
+    jump_force = 5;
+}
+
+function load_data()
+{
+    load_player();
 }
 
 function build_config()
 {
-	canv = document.getElementById("game");
-	ctx = canv.getContext("2d");
+    canv = document.getElementById("game");
+    ctx = canv.getContext("2d");
 }
 
 function update(progress)
 {
-	console.log(progress);
+    console.log(progress);
 }
 
 function render()
 {
-	ctx.fillRect(player_x, player_y, 30, 50);
+    ctx.fillRect(player_x, player_y, player_width, player_height);
 }
 
 function gameloop(timestamp)
 {
-	var progress = timestamp - lastRender;
+    var progress = timestamp - lastRender;
 
-	update(progress);
-	render();
+    update(progress);
+    render();
 
-	lastRender = timestamp;
+    lastRender = timestamp;
 
-	window.requestAnimationFrame(gameloop)
+    window.requestAnimationFrame(gameloop)
 }
 
 function generate_scene()
 {
-	build_config();
-	load_default_assests();
-	window.requestAnimationFrame(gameloop)
+    build_config();
+    load_data();
+    window.requestAnimationFrame(gameloop)
 }
 
 
