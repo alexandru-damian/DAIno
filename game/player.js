0c88053590bbbc4335cb97ab0c7297297d4a3033
Alexandru Damian <alexdamian121@gmail.com>
2019-06-24 00:42:11 +0300

    Added player basic class encapsulation

---
+++ game/player.js
@@ -0,0 +1,15 @@
+class Player {
+
+    this.#y_velocity = 0;
+    this.#Y_ACCELERATION = -15; 
+
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+
+    this.width = width;
+    this.height = height;
+  }
+
+}
+
