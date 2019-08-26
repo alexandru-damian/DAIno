d31ffebab4eb6c90abd909a34328d08def5099d2
Alexandru Damian <alexdamian121@yahoo.com>
2019-08-26 20:34:04 +0300

    Implemented node constructor with value as parameter

---
+++ ml/NEAT/src/node.py
@@ -0,0 +1,5 @@
+class Node:
+    value: int
+
+    def __init__(self, value = 0):
+        self.value = value
---
+++ ml/NEAT/test/test_node.py
@@ -0,0 +1,18 @@
+import unittest
+from src import node
+
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        self.node = node.Node()
+
+    def test_node_without_specified_value_must_be_zero(self):
+        self.assertEqual(self.node.value, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
