629becd8191d3cdda4938c6f6fe67e7dac5ba5de
Alexandru Damian <alexdamian121@yahoo.com>
2019-09-12 14:43:08 +0300

    Added exception when nodes have the same id

--- ml/NEAT/neat/gene.py
+++ ml/NEAT/neat/gene.py
@@ -1,5 +1,12 @@
+from neat import node
+
+
 class Gene:
-    def __init__(self, in_node, out_node):
+    def __init__(self, in_node: node.Node, out_node: node.Node):
+
+        if in_node.get_id() == out_node.get_id():
+            raise Exception('In and out node must be different')
+
         self.__in_node = in_node
         self.__out_node = out_node
 
--- ml/NEAT/test/test_gene.py
+++ ml/NEAT/test/test_gene.py
@@ -10,3 +10,8 @@ class TestGene(unittest.TestCase):
         in_node, out_node = dummy_gene.get_in_out_nodes()
         self.assertNotEqual(in_node.get_id(), out_node.get_id())
 
+    def test_expect_to_fail_if_in_and_out_nodes_are_the_same(self):
+        dummy_node = node.Node()
+        with self.assertRaises(Exception):
+            gene.Gene(dummy_node, dummy_node)
+
