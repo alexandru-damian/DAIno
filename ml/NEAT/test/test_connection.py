126d87c9a819e3dd55ac288af7d0f5e42beecd0d
Alexandru Damian <alexdamian121@yahoo.com>
2019-11-21 15:41:54 +0200

    Switched dummies with mocks

--- ml/NEAT/test/test_connection.py
+++ ml/NEAT/test/test_connection.py
@@ -1,41 +1,59 @@
 import unittest
+from unittest.mock import patch
 
 from neat import node, connection
 
 
 class TestConnection(unittest.TestCase):
 
-    def test_in_and_out_node_should_be_different(self):
-        dummy_connection = connection.Connection(node.Node().get_id(), node.Node().get_id())
+    @patch('neat.node.Node')
+    def setupNodeMock(self, node_mock_):
+        return node_mock_.return_value
 
-        in_node_id, out_node_id = dummy_connection.get_in_out_nodes()
-        self.assertNotEqual(in_node_id, out_node_id)
+    def setupMockObjectGetId(self, obj, id_: int):
+        obj.get_id.return_value = id_
+
+    def setupCreateDefaultConnection(self):
+        self.setupMockObjectGetId(self.__in_node, 0)
+        self.setupMockObjectGetId(self.__out_node, 1)
+
+        return connection.Connection(self.__in_node.get_id(), self.__out_node.get_id())
+
+    def setUp(self) -> None:
+        self.__in_node = self.setupNodeMock()
+        self.__out_node = self.setupNodeMock()
+
+        self.__connection = None
+
+    def test_in_and_out_node_should_be_different(self):
+        self.setupCreateDefaultConnection()
 
     def test_expect_to_fail_if_in_and_out_nodes_are_the_same(self):
-        dummy_node = node.Node()
+        self.setupMockObjectGetId(self.__in_node, 0)
+
         with self.assertRaises(Exception):
-            connection.Connection(dummy_node.get_id(), dummy_node.get_id())
+            connection.Connection(self.__in_node.get_id(), self.__in_node.get_id())
 
     def test_connection_by_default_has_status_enabled(self):
-        dummy_connection = connection.Connection(node.Node().get_id(), node.Node().get_id())
+        self.__connection = self.setupCreateDefaultConnection()
 
-        self.assertTrue(dummy_connection.get_status())
+        self.assertTrue(self.__connection.get_status())
 
     def test_weight_is_in_interval_for_100_connections(self):
 
         for index in range(0, 100):
-            connection_weight = connection.Connection(node.Node().get_id(), node.Node().get_id()).get_weight()
+            connection_weight = self.setupCreateDefaultConnection().get_weight()
 
             self.assertGreaterEqual(connection_weight, 0)
             self.assertLess(connection_weight, 1)
 
     def test_adding_two_new_connections_should_not_have_same_id(self):
 
-        first_dummy_connection = connection.Connection(node.Node().get_id(), node.Node().get_id())
-        second_dummy_connection = connection.Connection(node.Node().get_id(), node.Node().get_id())
+        self.__connection = self.setupCreateDefaultConnection()
+        dummy_connection = self.setupCreateDefaultConnection()
 
-        self.assertNotEqual(first_dummy_connection.get_id(), second_dummy_connection.get_id())
-        self.assertEqual(second_dummy_connection.get_id(), connection.Connection.get_last_id())
+        self.assertNotEqual(self.__connection.get_id(), dummy_connection.get_id())
+        self.assertEqual(dummy_connection.get_id(), connection.Connection.get_last_id())
 
 
 
