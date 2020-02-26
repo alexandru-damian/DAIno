6fd0e9ae8a674cdf2303a7bc7b60e3fce8fc9d6f
Alexandru Damian <alexdamian121@yahoo.com>
2020-02-26 17:07:56 +0200

    Added type to node

--- ml/NEAT/neat/node.py
+++ ml/NEAT/neat/node.py
@@ -1,16 +1,25 @@
 import numbers
 from neat import gene
+from enum import Enum
+
+
+class Type(Enum):
+    HIDDEN = 0
+    IN = 1
+    OUT = 2
 
 
 class Node(gene.Gene):
     __id = 0
     __nodes = dict()
 
-    def __init__(self, value: numbers.Number = 0):
+    def __init__(self, value: numbers.Number = 0, type_: Type = Type.HIDDEN):
         self.__value = value
 
         Node.__id += 1
         self.__current_id = Node.__id
+
+        self.__type = type_
 
     def connect(self, connection_id, in_node_id) -> bool:
         if connection_id is None or in_node_id is None:
@@ -28,6 +37,9 @@ class Node(gene.Gene):
 
         self.__value = value
         return True
+
+    def get_type(self) -> Type:
+        return self.__type
 
     def get_id(self) -> int:
         return self.__current_id
