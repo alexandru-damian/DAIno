9dd83b214122c959f9435bb52851727c3ce58d23
Alexandru Damian <alexdamian121@gmail.com>
2020-02-27 05:41:17 +0200

    Update genotype functionality to change nodes according to genoms

--- ml/NEAT/neat/genotype.py
+++ ml/NEAT/neat/genotype.py
@@ -1,24 +1,54 @@
 from neat import connection as conn
+from neat import node
 
 
 class Genotype:
+    class RefCounter:
+        def __init__(self):
+            self.__counter: int = 0
+
+        def increment(self):
+            self.__counter += 1
+
+        def decrement(self):
+            if self.__counter > 0:
+                self.__counter -= 1
+
+        def get_counter(self) -> int:
+            return self.__counter
 
     def __init__(self):
-
         self.__nodes = dict()
         self.__genomes = set()
 
     def add(self, genome: conn.Connection):
         self.__genomes.add(genome)
 
+        # unpack tuple into separated arguments
+        self.__add_node(genome.get_in_out_nodes())
+
     def remove(self, genome: conn.Connection):
         self.__genomes.remove(genome)
+        self.__remove_node(genome.get_in_out_nodes())
 
-    def update(self, genome: conn.Connection):
-        self.__genomes.remove(genome)
-        self.__genomes.add(genome)
+    def __add_node(self, nodes: [int, int]):
+        for elem in nodes:
+            ref_counter = self.__nodes.get(elem)
 
-#    def __update_nodes(self, genome : conn.Connection):
+            if ref_counter is None:
+                self.__nodes[elem] = Genotype.RefCounter()
+            else:
+                self.__nodes[elem].increment()
 
+    def __remove_node(self, nodes: [int, int]) -> bool:
+        for elem in nodes:
+            node_ref_counter: Genotype.RefCounter = self.__nodes.get(elem)
 
+            node_ref_counter.decrement()
+            if node_ref_counter.get_counter() is 0:
+                del self.__nodes[elem]
+                return True
+        return False
 
+    def get_nodes(self):
+        return self.__nodes
